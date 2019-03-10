00decf83236fc545d5e66433bbafd3900bae112c
shuchen <shuchen@fangdd.com>
2019-03-10 22:17:41 +0800

    install cache

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
+var CACHE_NAME = 'my-site-cache-v3';
+var urlsToCache = [
+  '/index.html',
+  'style.css',
+];
+
+
 self.addEventListener('install', function(event) {
   console.log('install');
+  // self.skipWaiting()
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 
